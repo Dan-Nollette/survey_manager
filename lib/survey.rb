30b72cfcca57bef74fe0dbd8cd51932116307aba
Dan Nollette and Snow Vilay <dan@>
2017-09-26 11:16:19 -0700

    add method to capitalize first letter of of each word in the survey title using a callback

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,4 +1,18 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
   validates(:title, :presence => true)
+  before_save(:upcase_title)
+
+private
+
+  def upcase_title()
+    title_arr = self.title.split(' ')
+    return_arr= []
+    title_arr.each do |word|
+      word_arr = word.split('')
+      word_arr[0].upcase!
+      return_arr.push(word_arr.join())
+    end
+    self.title = return_arr.join(' ')
+  end
 end
